d080c05af09216846771e91e09e67be13a7dc330
martanetza <39929716+martanetza@users.noreply.github.com>
2020-05-04 17:37:45 +0200

    fixing locks

--- single_course.php
+++ single_course.php
@@ -1,16 +1,26 @@
 <?php
 require_once('db_conn.php');
 
+$user_id = 'f@f.com';
 $course_id = $_GET['course_id'];
 $test_template = file_get_contents("test-template.html");
 ?>
 <?php
 
 try {
+  $query_chosen_course = $conn->prepare('SELECT progress FROM chosen_course WHERE course_id = :course_id AND user_email = :user_email');
+  $query_chosen_course->bindValue(':course_id', $course_id);
+  $query_chosen_course->bindValue(':user_email', $user_id);
+
+  $query_chosen_course->execute();
+  $row_chosen_course = $query_chosen_course->fetchAll();
+  $progress = $row_chosen_course[0]->progress;
 
   $q_module = $conn->prepare('SELECT * FROM modules WHERE course_id=' . $course_id);
   $q_module->execute();
   $modules_rows = $q_module->fetchAll();
+  $count =  $q_module->rowCount();
+  $open_lock_number = $progress / (100 / $count);
 
 
   $q_module_join = $conn->prepare('SELECT * FROM items LEFT JOIN modules ON items.module_id = modules.module_id WHERE course_id=' . $course_id);
@@ -56,27 +66,41 @@ try {
     <main class="single-course-main">
       <div class="single-course-list">
         <div class="single-course-list-wrap">
-          <?php foreach ($modules_rows as $row) : ?>
+          <?php foreach ($modules_rows as $key => $row) : ?>
             <?php
             $q_items = $conn->prepare('SELECT * FROM items WHERE module_id=' . $row->module_id);
             $q_items->execute();
             $items_rows = $q_items->fetchAll();
+
             ?>
             <div class="module-list-element">
-              <div onclick="openContent(<?= $row->module_id; ?>)" class="module-list-element-header">
-                <div><?= $row->module_name; ?></div>
+              <?php
+              if ($key <= $open_lock_number - 1) {
+                $locked = 0;
+                $lock = '<i class="fas fa-lock-open"></i>';
+              } else {
+                $locked = 1;
+                $lock = '<i class="fas fa-lock"></i>';
+              }
+              ?>
+              <div onclick="openContent(<?= $row->module_id . ',' . $locked; ?>)" id="header-module-<?= $row->module_id; ?>" class="module-list-element-header">
+                <div class="name-container">
+                  <div class="lock"><?= $lock; ?></div>
+                  <div><?= $row->module_name; ?></div>
+                </div>
                 <i class="fas fa-angle-right fa-1x"></i>
               </div>
               <div id="content-module-<?= $row->module_id; ?>" class="module-list-element-content">
                 <?php
                 foreach ($items_rows as $row) :
                 ?>
+
                   <div onclick="fetchSingleItemContent(<?= $row->item_id; ?>)" class="item-list-element">
                     <div><?= $row->item_title; ?></div>
                     <i class="fas fa-arrow-right"></i>
                   </div>
                 <?php endforeach; ?>
-                <div onclick="fetchTest(<?= $row->module_id . ',' . $course_id ?>)" class="item-list-element">
+                <div onclick="fetchTest(<?= $row->module_id . ',' . $course_id . ',' . $modules_rows[$key + 1]->module_id; ?>)" class="item-list-element">
                   <div>Test</div>
                   <i class="fas fa-arrow-right"></i>
                 </div>
@@ -117,9 +141,11 @@ try {
 <script>
   document.querySelector(".module-list-element-content").classList.add("show");
 
-  function openContent(moduleId) {
-    console.log("test")
-    document.querySelector("#content-module-" + moduleId).classList.toggle("show");
+  function openContent(moduleId, locked) {
+    console.log(event.target)
+    if (locked == 0) {
+      document.querySelector("#content-module-" + moduleId).classList.toggle("show");
+    }
   }
 
 
@@ -136,7 +162,7 @@ try {
   }
 
 
-  function fetchTest(module_id, test_id) {
+  function fetchTest(module_id, course_id, next_module_id) {
     var test_template = `<?= $test_template; ?>`
     var test_template_copy = test_template;
     console.log(module_id)
@@ -153,7 +179,7 @@ try {
       document.querySelector('.modal').innerHTML = test_template_copy;
       controlCheckboxes()
       document.querySelector('.submit-btn').addEventListener('click', () => {
-        validateTheAnswer(jData, test_id)
+        validateTheAnswer(jData, course_id, next_module_id)
       })
     }
     fetchingDataTest(module_id)
@@ -174,13 +200,18 @@ try {
     })
   }
 
-  function validateTheAnswer(data, testID) {
+  function validateTheAnswer(data, courseID, next_module_id) {
     var allOptions = document.querySelectorAll('#answer_A, #answer_B, #answer_C, #answer_D')
     allOptions.forEach(elm => {
-
+      console.log(elm.value, data[0].answer, elm.checked)
       if (elm.value == data[0].answer && elm.checked) {
         document.querySelector('.modal .test-container').innerHTML = '<div class="message"> <p>Congratualtions your answer was correct you can move now to the next module <p> </div>';
-        console.log(testID)
+        document.querySelector('#header-module-' + next_module_id).setAttribute('onclick', `openContent(${next_module_id},0)`);
+        document.querySelector('#header-module-' + next_module_id + ' .lock').innerHTML = '<i class="fas fa-lock-open"></i>'
+        console.log(document.querySelector('#header-module-' + next_module_id))
+      } else if (elm.value !== data[0].answer && elm.checked) {
+        document.querySelector('.modal .test-container').innerHTML = '<div class="message"> <p>Wrong<p> </div>';
+
       }
 
     })
